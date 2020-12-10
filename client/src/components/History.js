bf39ee0e9984b81b78b12e9c5b2a41404f26d05a
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-12-10 01:09:05 -0500

    History

--- client/src/components/History.js
+++ client/src/components/History.js
@@ -1,13 +1,12 @@
 import React from 'react'
+import Transactions from './Transactions'
 
 const History = () => {
     return (
         <div>
              <h3>History</h3>
       <ul id="list" class="list">
-         <li class="minus">
-          Cash <span>-$400</span><button class="delete-btn">x</button>
-        </li>
+        <Transactions/>
       </ul>
         </div>
     )
---
+++ client/src/components/Transactions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Transactions = () => {
+    return (
+        <li class="minus">
+        Cash <span>-$400</span><button class="delete-btn">x</button>
+      </li>
+    )
+}
+
+export default Transactions
