022391683e2b750e7e1e1de6d1b162ddc801ae11
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-12-10 00:59:02 -0500

    income

---
+++ client/src/components/Account.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './Header'
+import Balance from './Balance'
+
+export const Account = () => {
+    return (
+        <div>
+            <Header/>
+            <Balance/>
+            
+        </div>
+    )
+}
+
+export default Account;
