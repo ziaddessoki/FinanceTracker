102f3ce79ae5e810bbd10fcce8dbaee8f9607693
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-12-10 01:02:18 -0500

    income

--- client/src/components/Account.js
+++ client/src/components/Account.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Header from './Header'
 import Balance from './Balance'
+import IncomeExpense from './IncomeExpense'
 
 export const Account = () => {
     return (
         <div>
             <Header/>
             <Balance/>
-            
+            <IncomeExpense/>
         </div>
     )
 }
