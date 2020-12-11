5a720ebfd8b407dbf1fd39f2fc9d23cd63599966
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-12-11 04:51:19 -0500

    new Trans

--- client/src/components/NewTransactions.js
+++ client/src/components/NewTransactions.js
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const NewTransactions = () => {
   const [text, setText] = useState('')
-  const [amount, setAmount] = useState(0)
+  let [amount, setAmount] = useState(0)
   const { addTransaction } = useContext(GlobalContext);
 
   const onSubmit = e => {
@@ -32,12 +32,15 @@ const NewTransactions = () => {
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label htmlFor="amount">Amount <br />
-            (negative - expense, positive - income)
-            </label>
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+          <label htmlFor="amount">Amount</label>
+          <input type="number" name="amount" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
-        <button className="btn">Add transaction</button>
+        {/* <button className="btn">Add transaction</button> */}
+        <div className="buttons">
+          <button className="btn incomeBtn"><i className="fas fa-plus-circle"></i> Income</button>
+          <button className="btn expenseBtn" onClick={() => amount = -amount}><i className="fas fa-minus-circle"></i> Expense</button>
+        </div>
+
       </form>
     </div>
   )
