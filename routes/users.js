3437abaeeeec67d3568c2818579fa6b447b18ebb
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2020-12-29 18:49:34 -0500

    backend

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,8 @@ const User = require('../models/users')
 
 // router.get('/', (req, res) => res.send("HELL YeaH!!!"));
 
-//@desc Get all transactions
-//@route GET /api/v1/transactions
+//@desc Get all users
+//@route GET /api/v1/user
 //@access Public
 //to use the middleware just add the file as a second paramter and route will be protected
 router.get("/", async (req, res) => {
@@ -17,20 +17,37 @@ router.get("/", async (req, res) => {
         // not returning password in json
         //   const user = await User.findById(req.user.id).select("-password");
         //   res.json(user);
-        res.send('Get Transactions')
+        res.send('Get users')
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+//@desc Get user by id
+//@route GET /api/v1/user/:id
+//@access Public
+//to use the middleware just add the file as a second paramter and route will be protected
+router.get("/:id", async (req, res) => {
+    try {
+        //since its a protected route n we use the token that has the ID
+        //in middleware we set the req.user to the user w/token
+        // not returning password in json
+        //   const user = await User.findById(req.user.id).select("-password");
+        //   res.json(user);
+        res.send('Get user')
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
     }
 });
 
-///@desc  Add transaction
-//@route POST /api/v1/transactions
+///@desc  Add user
+//@route POST /api/v1/user
 //@access Public
 //to use the middleware just add the file as a second paramter and route will be protected
 router.post("/", async (req, res) => {
     try {
-        res.send('Post Transactions')
+        res.send('Post User')
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
@@ -38,12 +55,12 @@ router.post("/", async (req, res) => {
 });
 
 
-//@desc Delete transactions
-//@route DELETE /api/v1/transactions/:id
+//@desc Delete User
+//@route DELETE /api/v1/user/:id
 //@access Public
 router.delete("/:id", async (req, res) => {
     try {
-        res.send('delete Transactions')
+        res.send('delete User')
     } catch (err) {
         console.error(err.message);
         res.status(500).send("Server Error");
