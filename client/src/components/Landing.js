ca4149ebc6b8f6f1091b19a213e937b9ead92d81
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2021-03-17 23:19:24 -0400

    landing

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,27 +6,23 @@ import cash2 from '../assets/cash-clear.gif'
 const Landing = () => {
     return (
         <div>
-            <p1>
-                Experience a
-                fresh way to
-                manage money
-        </p1>
+            <h7>Experience a way to manage money</h7>
             <br />
             <p1>Reach your goals with personalized insights,
             custom budgets, spend tracking, and subscription
             monitoring.</p1>
 
-            <img src={hand} alt="hand" />
+            <img src={hand} alt="hand" className="imgLanding" />
 
             <br />
             <h7>Stay focused on your financial goals.</h7>
-
-            <h7>
+            <br />
+            <p1>
                 Improve your spending habits with custom
                 goals that keep you going. Save for a home,
                 crush debt, and prepare for the future.
-            </h7>
-            <img src={cash2} alt="hand" />
+            </p1>
+            <img src={cash2} alt="hand" className="imgLanding" />
         </div>
     )
 }
