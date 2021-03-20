04b49a91ed035007985eaae3bf818db72d9db741
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2021-03-20 16:58:34 -0400

    landing

--- routes/stocks.js
+++ routes/stocks.js
@@ -43,14 +43,16 @@ router.get("/", async (req, res) => {
 //@desc Get stock time series (for chart) 
 //@route GET /api/v1/stocks
 //@access Public
+//sym = symbol 
+//interval = [1,5,15,30,60]min
 //to use the middleware just add the file as a second paramter and route will be protected
-router.get("/alpha/:sym", async (req, res) => {
+router.get("/alpha/:sym/:int", async (req, res) => {
     try {
         // const stock = await finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, {}, (error, data, response) => {
         //     console.log(data)
         // });
         console.log(alpha_apiKey)
-        const stock = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${req.params.sym}&interval=5min&apikey=${alpha_apiKey}`)
+        const stock = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${req.params.sym}&interval=${int}min&apikey=${alpha_apiKey}`)
         console.log(stock)
         return res.status(200).json({
             success: true,
@@ -69,5 +71,65 @@ router.get("/alpha/:sym", async (req, res) => {
     }
 })
 
+//@desc Get stock quote 
+//@route GET /api/v1/stocks/quote
+//@access Public
+//sym = symbol 
+
+//to use the middleware just add the file as a second paramter and route will be protected
+router.get("/quote/:sym", async (req, res) => {
+    try {
+        // const stock = await finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, {}, (error, data, response) => {
+        //     console.log(data)
+        // });
+        console.log(alpha_apiKey)
+        const stock = await axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.params.sym}&apikey=${alpha_apiKey}`)
+        console.log(stock)
+        return res.status(200).json({
+            success: true,
+            Api: "alpha",
+            count: stock.data.length,
+            data: stock.data
+        })
+
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: "Server Error"
+        })
+    }
+})
+
+//@desc Get company info
+//@route GET /api/v1/stocks/quote
+//@access Public
+//sym = symbol 
+
+//to use the middleware just add the file as a second paramter and route will be protected
+router.get("/company/:sym", async (req, res) => {
+    try {
+        // const stock = await finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, {}, (error, data, response) => {
+        //     console.log(data)
+        // });
+        console.log(alpha_apiKey)
+        const stock = await axios.get(`https://www.alphavantage.co/query?function=OVERVIEW&symbol=${req.params.sym}&apikey=${alpha_apiKey}`)
+        console.log(stock)
+        return res.status(200).json({
+            success: true,
+            count: stock.data.length,
+            data: stock.data
+        })
+
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: "Server Error"
+        })
+    }
+})
 
 module.exports = router;
