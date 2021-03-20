e298486fe9156a04efba81490fb583a95f61994f
Ziad Eldesouky <ziad.dessoki@yahoo.com>
2021-03-20 01:11:07 -0400

    landing

--- routes/stocks.js
+++ routes/stocks.js
@@ -7,14 +7,13 @@ const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = process.env.FINNHUB_APIKEY // Replace this
 const finnhubClient = new finnhub.DefaultApi()
 
-// console.log(process.env.FINNHUB_APIKEY)
+const alpha_apiKey = process.env.ALPHA_APIKEY
 
-// const stock = finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, {}, (error, data, response) => {
-//     console.log(data)
-// });
 
-// console.log(stock)
-
+//@desc Get search for stocks 
+//@route GET /api/v1/stocks
+//@access Public
+//to use the middleware just add the file as a second paramter and route will be protected
 router.get("/", async (req, res) => {
     try {
         // const stock = await finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, {}, (error, data, response) => {
@@ -26,6 +25,36 @@ router.get("/", async (req, res) => {
         return res.status(200).json({
             success: true,
             luck: "non",
+            count: stock.data.result.length,
+            data: stock.data.result
+        })
+
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            error: "Server Error"
+        })
+    }
+})
+
+
+//@desc Get search for stocks 
+//@route GET /api/v1/stocks
+//@access Public
+//to use the middleware just add the file as a second paramter and route will be protected
+router.get("/alpha", async (req, res) => {
+    try {
+        // const stock = await finnhubClient.stockCandles("AAPL", "D", 1590988249, 1591852249, {}, (error, data, response) => {
+        //     console.log(data)
+        // });
+
+        const stock = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${alpha_apiKey}`)
+        console.log(stock)
+        return res.status(200).json({
+            success: true,
+            Api: "aplha",
             count: stock.data.result.length,
             data: stock.data.result
         })
